d52b41797c36ab70fc6b0e72b5a5879ea4b42086
tomiyasu yuya <tomi.tomi.tomi.09@icloud.com>
2023-08-07 23:05:43 +0900

    add:weather

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,6 +1,17 @@
 class WeathersController < ApplicationController
   
   def seach
-    
+
   end
+
+  def show
+    lat_id = params[:lat_id]
+    lon_id = params[:lon_id]
+    weather = URI.parse("http://api.openweathermap.org/data/2.5/weather?lat=#{lat_id}&lon=#{lon_id}&APPID=#{ENV['API_KEY']}")
+    response = Net::HTTP.get(weather)
+    weather_list = JSON.parse(response)
+    tenki = weather_list["main"]
+    @tenki = tenki
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'seach', to: "weathers#seach"
+
+  get 'show', to: 'weathers#show'
 end
